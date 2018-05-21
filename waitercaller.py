5ee15697239d242be70c71873c4a598503739236
pangqiqiang <78733149@qq.com>
2018-05-22 00:37:12 +0800

    add bootstap login

--- waitercaller.py
+++ waitercaller.py
@@ -1,10 +1,51 @@
 from flask import Flask
+from flask import render_template
+from flask_login import LoginManager
+from flask_login import login_required
+from flask_login import login_user
+from flask import url_for
+from flask import redirect
+from flask import request
+from mockdbhelper import MockDBHelper as DBHelper
+from user import User
+
 
 app = Flask(__name__)
+login_manager = LoginManager(app)
+DB = DBHelper()
+
+app.secret_key = 'tPXJY3X37Qybz4QykV+hOyUxVQeEXf1Ao2C8upz+fGQXKsM'
+
 
 @app.route("/")
 def home():
-    return "Under construction"
+    return render_template("home.html")
+
+
+@app.route("/account")
+@login_required
+def account():
+    return "You are logged in"
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    user_password = DB.get_user(email)
+    if user_password and user_password == password:
+        user = User(email)
+        login_user(user, remember=True)
+        return redirect(url_for('account'))
+    return home()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    user_password = DB.get_user(user_id)
+    if user_password:
+        return User(user_id)
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
